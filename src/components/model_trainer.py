753d49de07bff50dd62ea1b08acfe84bdbe0bf3f
Muhammad Asif <muasif025@gmail.com>
2026-02-08 18:27:18 +0500

    model trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,74 @@
+import os 
+import sys 
+from dataclasses import dataclass
+
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+        
+    def initiate_model_trainer(self, train_arr, test_arr):
+        try:
+            logging.info("splitting train and test data")
+            x_train,y_train,x_test,y_test=(
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+            
+            models = {
+                "Linear_Regression": LinearRegression(),
+                "Gradient_Boosting": GradientBoostingRegressor(),
+                "KNRegressor":KNeighborsRegressor(),
+                "Decision_Tree":DecisionTreeRegressor(),
+                "Random_forest_regressor":RandomForestRegressor(),
+                "AdaBoostRegressor":AdaBoostRegressor()
+            }
+            
+            model_result_report:dict=evaluate_models(x_train,y_train,x_test,y_test, models)
+            
+            best_r2_score = max(sorted(model_result_report.values()))
+            
+            best_model_name = list(models.keys())[
+                list(model_result_report.values()).index(best_r2_score)
+            ]
+            best_model = models[best_model_name]
+            
+            if best_r2_score <.6:
+                raise CustomException("Not best model found",sys)
+                logging.info("Best model not found")
+            logging.info("best r2_scored model found which is {}".format(best_model_name))
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            
+            y_pred=best_model.predict(x_test)
+            
+            r2_scores=r2_score(y_test, y_pred)
+            
+            return r2_scores
+            
+            
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,17 @@ import numpy as np
 import dill
 from src.exception import CustomException
 
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+
+from sklearn.metrics import r2_score
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -13,5 +24,23 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file:
             dill.dump(obj, file)
     
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def evaluate_models(x_train,y_train,x_test,y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model = model.fit(x_train,y_train)
+            y_pred= model.predict(x_test)
+            r2_scores = r2_score(y_test,y_pred)
+            
+            report[list(models.keys())[i]]=r2_scores
+            
+        return report
+        
     except Exception as e:
         raise CustomException(e,sys)
